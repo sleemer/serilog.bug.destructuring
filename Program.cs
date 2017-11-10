f4759c29117bb059097fd622ea8075e304e49e99
vlad <thesleemer@icloud.com>
2017-11-10 12:43:54 +0100

    Initial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Serilog;
+
+namespace serilog.bug.destructuring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Destructure.ByTransformingWhere<Type>(type => true, type => new { FullName = type.FullName })
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Information("{@Type}", typeof(string));
+        }
+    }
+}
